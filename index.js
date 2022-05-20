b4900335743f2ed421b7e8a40c711b1586762ae3
vijayEelumalai <102545971+vijayEelumalai@users.noreply.github.com>
2022-05-20 13:51:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// for(let i=0; i < 50; i++){
+//     if(i % 2 !== 0){
+//         console.log(i);
+//     }
+// }
+// const array = [1,2,3,true, false, null, undefined, ''];
+// const value = array.filter(val =>{
+//     return val;
+// })
+// console.log(value);
+
+const array=[2,4,5,6,8,2,5,4,7,3,5];
+const value = array.reverse();
+console.log(value);
+
+
